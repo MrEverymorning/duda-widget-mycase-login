0fe329841d9a6c96b7fb1c5aeba2f3b0e1c82ed5
markprictor <mark.prictor@appfolio.com>
2019-11-15 15:41:24 -0800

    Initial test commit

---
+++ src/duda.js
@@ -0,0 +1,32 @@
+//Get MyCase JS
+dmAPI.loadScript('https://login.mycase.com/customer_websites/login_javascript.js').done(() => {
+	console.log('External scripts loaded successfully.');
+});
+
+//Show Modal
+function showModal() {
+	$('#modal').addClass('show-modal');
+	$('body').addClass('body-fix');
+}
+
+//Close Modal
+function closeModal() {
+	$('#modal').removeClass('show-modal');
+	$('body').removeClass('body-fix');
+}
+
+//Show modal on contact us button click
+$(document).on('click', '#mycase-login-btn', e => {
+	e.preventDefault();
+	showModal();
+});
+
+//Close modal
+$(document).on('click', '#close-modal', e => {
+	e.preventDefault();
+	closeModal();
+});
+
+$(document).on('click', '.modal-overlay', () => {
+	closeModal();
+});
