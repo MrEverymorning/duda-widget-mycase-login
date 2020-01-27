68da02acfcc72360758a25e375ab0d9e0acb8a33
Mark Prictor <mark.prictor@appfolio.com>
2020-01-27 10:50:02 -0800

    Add custom modal title variable & UUID

--- src/duda.js
+++ src/duda.js
@@ -1,18 +1,32 @@
-//Get MyCase JS
-dmAPI.loadScript('https://login.mycase.com/customer_websites/login_javascript.js').done(() => {
-	console.log('External scripts loaded successfully.');
-});
+//START DUDA WIDGET
+
+//Load NPM dependencies
+const bodyScrollLock = require('body-scroll-lock');
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
+
+getFirmUUID();
+
+//Get Firm UUID
+function getFirmUUID() {
+	let firmUUID = data.config.firm_uuid.includes('<p class="rteBlock">') ? jQuery(data.config.firm_uuid).text() : data.config.firm_uuid;
+	$.getScript('https://login.mycase.com/customer_websites/login_javascript.js').done(() => {
+		$('#mycase-login-form').attr('data-firm-id', firmUUID);
+	});
+}
 
 //Show Modal
 function showModal() {
-	$('#modal').addClass('show-modal');
-	$('body').addClass('body-fix');
+	$('#modal')
+		.addClass('show-modal')
+		.appendTo($('body'));
+	disableBodyScroll(document.querySelector('#modal'));
 }
 
 //Close Modal
 function closeModal() {
 	$('#modal').removeClass('show-modal');
-	$('body').removeClass('body-fix');
+	enableBodyScroll(document.querySelector('#modal'));
 }
 
 //Show modal on contact us button click
--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,40 @@
-//Get MyCase JS
-$.getScript('https://login.mycase.com/customer_websites/login_javascript.js').done(() => {
-	console.log('External scripts loaded successfully.');
-});
+//Duda Config
+const data = {
+	config: {
+		loginButtonText: 'Client Login',
+		firm_uuid: '<p class="rteBlock">8411ad35-a3ae-44d7-9a69-9637635f425a</p>'
+	}
+};
+
+//START DUDA WIDGET
+
+//Load NPM dependencies
+const bodyScrollLock = require('body-scroll-lock');
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
+
+getFirmUUID();
+
+//Get Firm UUID
+function getFirmUUID() {
+	let firmUUID = data.config.firm_uuid.includes('<p class="rteBlock">') ? jQuery(data.config.firm_uuid).text() : data.config.firm_uuid;
+	$.getScript('https://login.mycase.com/customer_websites/login_javascript.js').done(() => {
+		$('#mycase-login-form').attr('data-firm-id', firmUUID);
+	});
+}
 
 //Show Modal
 function showModal() {
-	$('#modal').addClass('show-modal');
-	$('body').addClass('body-fix');
+	$('#modal')
+		.addClass('show-modal')
+		.appendTo($('body'));
+	disableBodyScroll(document.querySelector('#modal'));
 }
 
 //Close Modal
 function closeModal() {
 	$('#modal').removeClass('show-modal');
-	$('body').removeClass('body-fix');
+	enableBodyScroll(document.querySelector('#modal'));
 }
 
 //Show modal on contact us button click
